7d92aa0e9e08d8a32f8a5e675f9b9e9dc3130477
Caique L. Fernandes <caique.l.f9.clf@gmail.com>
2024-05-09 15:37:55 -0400

    implementando post para users

---
+++ src/users/users.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private readonly users : User[] = [];
+  private id = 1;
+
+  create(createUserDto: CreateUserDto) {
+    const newUser = {
+      id : this.id,
+      name : createUserDto.name,
+      email : createUserDto.email,
+      password : createUserDto.password
+    }
+    this.id = this.id + 1;
+
+    this.users.push(newUser);
+
+    const{password, ...usercreated} = newUser
+
+    return usercreated;
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
